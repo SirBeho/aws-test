7e2238abec3c50421e5b13f12c3aa8744429ce18
SirBeho <benjamin.tavarez.98@gmail.com>
2024-06-24 08:15:39 -0400

    fist

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Desplegando la aplicación NestJS a producción..."
+
+# Aquí puedes añadir comandos para desplegar a tu servidor en la nube
+# Por ejemplo, para AWS Elastic Beanstalk:
+# eb init -p node.js my-app --region us-west-2
+# eb deploy
+
+echo "Despliegue de la aplicación NestJS completado."
